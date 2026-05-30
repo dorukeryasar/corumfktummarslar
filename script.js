fd17e63c192cac153745c7194852252f6600a3d8
dorukeryasar <dorukbabanpro@gmail.com>
2026-05-30 18:34:39 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const player = document.getElementById("player");
-
-function playSong() {
-  player.src = "https://drive.usercontent.google.com/u/0/uc?id=1xv5YJzYruYZikCoK6OTcbic0X8m3FYQ8&export=download";
-  player.play();
-}
